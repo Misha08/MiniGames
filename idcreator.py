aeb54b328dad2e5c9aa84925fb6b16bbdf95f9d2
Mihail <viktor.svorikin@gmail.com>
2022-11-29 23:48:52 +0100

    The new file, which generate the new id for 'Player' class

---
+++ idcreator.py
@@ -0,0 +1,19 @@
+from random import choice
+from string import ascii_lowercase, ascii_uppercase, digits
+
+general_collection = ascii_uppercase + ascii_lowercase + digits
+
+
+class Id(object):
+    def __init__(self):
+        self.__id = ""
+
+    def generate_id(self):
+        for _ in range(6):
+            self.__id += choice(general_collection)
+        print(self.__id)
+
+
+if __name__ == "__main__":
+    id = Id()
+    print(id.generate_id())
