e8a1e0f8f891c3d2a5af984e48bffe5764de7b53
Mihail <viktor.svorikin@gmail.com>
2022-11-30 11:52:54 +0100

    The low changes

--- HelloText.py
+++ HelloText.py
@@ -1,7 +1,3 @@
 def print_hello_text():
     print("/*____________________________Hello dear player! :)____________________________*/")
     print("I'm glad to welcome in my collection of mini-games, Here are the following games:")
-    print("1) Lottery")
-    print("2) Mini Casino")
-    print("3) Snake")
-    print("4) Nim")
