cd0ec44a7196dd3d47a15b42eac0578a3cc6e9a9
Mihail <viktor.svorikin@gmail.com>
2022-11-30 11:51:23 +0100

    The game init was added here

--- Games/Lottery.py
+++ Games/Lottery.py
@@ -1,20 +1,24 @@
+from random import randint
 from player import Player
 
 
 class Lottery(object):
-    def __init__(self, count_of_players=0, regime="4 from 20"):
-        self.count_of_players = count_of_players
+    def __init__(self, count_of_players=0, number_to_find=4, number_from_find=20):
         self.players = list()
+        self.count_of_players = count_of_players
+        self.number_to_find = number_to_find
+        self.number_from_find = number_from_find
 
     def init_players(self):
         string_of_count = ""
         for i in range(1, self.count_of_players + 1):
-            if i == 1:
-                string_of_count = f"{i}-st"
-            elif i == 2:
-                string_of_count = f"{i}-nd"
-            else:
-                string_of_count = f"{i}-th"
+            match i:
+                case 1:
+                    string_of_count = f"{i}-st"
+                case 2:
+                    string_of_count = f"{i}-nd"
+                case other:
+                    string_of_count = f"{i}-th"
 
             name = input(f"What's the name of the {string_of_count} player: ")
             surname = input(f"What's the name of the {string_of_count} player: ")
@@ -24,3 +28,11 @@ class Lottery(object):
 
     def get_players(self):
         return self.players
+
+    def play(self):
+        size_of_playing_field = self.number_from_find / self.number_to_find
+        # print("Now it's up to you which move will be first.")
+        # first = randint(1, len(self.players))
+
+        while True:
+            pass
