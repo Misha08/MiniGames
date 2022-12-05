cae9df12ab21b38dffa7fff839f5641b3c5a73d1
Mihail <viktor.svorikin@gmail.com>
2022-12-05 21:04:17 +0100

    improved algorithm for creating a classic, decorated field and also random number matching results

--- field_creation_algorithm.py
+++ field_creation_algorithm.py
@@ -5,7 +5,6 @@ def create_field(count_of_numbers: int):
 
     count = count_of_numbers
     square_of_count = int(sqrt(count))
-    tech_arr = [i for i in range(1, count + 1)]
     matrix_with_counts = [list() for _ in range(square_of_count)]
     begin = 0
     end = square_of_count
@@ -21,10 +20,10 @@ def create_field(count_of_numbers: int):
     if count_add == 0:
         matrix_with_counts.append([i for i in range(count - square_of_count + 2, count + 1)])
     else:
-        c1 = square_of_count ** 2 + 1
-        c2 = c1 + square_of_count
+        begin = square_of_count ** 2 + 1
+        end = begin + square_of_count
         for i in range(count_add):
-            matrix_with_counts.append([j for j in range(c1, c2)])
+            matrix_with_counts.append([j for j in range(begin, end)])
 
         matrix_with_counts.append([k for k in range(matrix_with_counts[-1][-1] + 1, count + 1)])
 
@@ -37,4 +36,33 @@ def create_field(count_of_numbers: int):
     for i in matrix_with_counts:
         print(*i)
 
-    return tech_arr, matrix_with_counts
+    return matrix_with_counts
+
+
+def get_beautiful_field(max_number, field, set_of_guess_numbers, result_turn):
+    field_for_beauty = field
+    sample_numbers = set(set_of_guess_numbers)
+    matching_numbers = set(result_turn)
+    set_of_matched_numbers = sample_numbers.intersection(matching_numbers)
+    beautiful_field = list()
+
+    for i in field_for_beauty:
+        beautiful_field.append([j.replace(" ", "") for j in i])
+
+    for k in set_of_matched_numbers:
+        for i in range(len(beautiful_field)):
+            for j in range(len(beautiful_field[i])):
+                if str(k) == beautiful_field[i][j]:
+                    beautiful_field[i][j] = "x" * len(beautiful_field[i][j])
+
+    for i in range(len(beautiful_field)):
+        for j in range(len(beautiful_field[i])):
+            beautiful_field[i][j] = (len(str(max_number)) - len(str(beautiful_field[i][j]))) * " "\
+                                    + str(beautiful_field[i][j])
+
+    return beautiful_field, set_of_matched_numbers
+
+
+def get_field(field):
+    for row in field:
+        print(*row)
