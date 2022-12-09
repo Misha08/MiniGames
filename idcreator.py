34bdd7f5e4d56c2a0aee5799d494335b5811ddcd
Mihail <viktor.svorikin@gmail.com>
2022-12-10 00:37:18 +0100

    Description added

--- idcreator.py
+++ idcreator.py
@@ -1,19 +1,22 @@
 from random import choice
 from string import ascii_lowercase, ascii_uppercase, digits
 
+# Collection of the ascii symbols
 general_collection = ascii_uppercase + ascii_lowercase + digits
 
 
+# ID class
 class Id(object):
+    # Initialization
     def __init__(self):
         self.__id = ""
 
+    # The function of generate ID
     def generate_id(self):
+
+        # Filling the id with the six random ascii characters
         for _ in range(6):
             self.__id += choice(general_collection)
+
+        # Return id for the player
         return self.__id
-
-
-if __name__ == "__main__":
-    id = Id()
-    print(id.generate_id())
