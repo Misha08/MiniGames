9d7f6404d26c89590594b21f31235560553a1ce9
Mihail <viktor.svorikin@gmail.com>
2022-12-10 00:36:28 +0100

    Description added

--- Games/Lottery.py
+++ Games/Lottery.py
@@ -3,53 +3,122 @@ from player import Player
 from field_creation_algorithm import *
 
 
+# Main game class
 class Lottery(object):
 
-    @classmethod
-    def validate_the_size_numbers(cls, count_of_ntf: int, count_of_nff: int):
-        if 4 > count_of_ntf or count_of_ntf > count_of_nff:
+    # The Validate methods
+    # Validation of input data to form the playing field
+    @staticmethod
+    def validate_the_size_numbers(count_of_ntf: int, count_of_nff: int):
+        '''
+
+        This function checks the input dimensions, the number of search numbers and field numbers,
+        and returns true or false depending on their compatibility with the requirements.
+
+        Allowed values for the formation of the correct field and the number of numbers to be searched:
+
+        a) For number of numbers to look for:
+        1) Minimum value: 4
+        2) Maximum value: The value of the 'Count of numbers on the playing field'
+
+        b) For number of numbers on the playing field:
+        1) Minimum value: 20
+        2) Maximum value: 131
+
+        !!! In case of data invalidity, the value false will be returned and
+        instead of creating an instance of the class (Object), an exception will be raised !!!
+
+        '''
+
+        # Checking the value of the number of numbers you are looking for
+        if count_of_ntf < 4 or count_of_nff < count_of_ntf:
             return False
 
+        # Checking the value of the number of numbers in the playing field
         if 20 > count_of_nff or count_of_nff > 131:
             return False
 
         return True
 
-    @classmethod
-    def validate_the_count_of_rounds_and_players(cls, players: int, rounds: int):
-        return (0 < rounds < 30) and (0 < players < 30)
+    # Validation of rounds in the game
+    @staticmethod
+    def validate_the_count_of_rounds_and_players(players: int, rounds: int):
+        '''
 
+        This function checks the number of rounds and players in the game
+
+        Allowed values for the count of players and the count of the rounds:
+
+        a) For players:
+        1) Minimum value: 1
+        2) Maximum value: 30
+
+        b) For rounds:
+        1) Minimum value: 1
+        2) Maximum value: 131
+
+        '''
+        return (0 < rounds <= 30) and (0 < players <= 30)
+
+    # Method for initialization The object of the class
     def __init__(self, count_of_players=0, number_to_find=4, number_from_find=20, rounds=1):
-        if (Lottery.validate_the_count_of_rounds_and_players(count_of_players, rounds) and
-                Lottery.validate_the_size_numbers(number_to_find, number_from_find)):
-            self.players = list()
-            self.count_of_players = count_of_players
-            self.number_to_find = number_to_find
-            self.number_from_find = number_from_find
-            self.rounds = rounds
-        else:
-            raise(TypeError("Your data is wrong"))
+        '''
 
+        Magic Initialization Method that creates an instance with parameters such as:
+
+        1) The Number of players in the game (count_of_players)
+        2) The Number of numbers to be found (number_to_find)
+        3) The Number of numbers from which these numbers will be searched (number_from_find)
+        4) The number of rounds (rounds)
+
+        '''
+        self.players = list()
+        self.count_of_players = count_of_players
+        self.number_to_find = number_to_find
+        self.number_from_find = number_from_find
+        self.rounds = rounds
+
+    # Function of initialization players
     def init_players(self):
-        for i in range(1, self.count_of_players + 1):
-            match i:
-                case 1:
-                    string_of_count = f"{i}-st"
-                case 2:
-                    string_of_count = f"{i}-nd"
-                case _:
-                    string_of_count = f"{i}-th"
+        '''
 
+        This function creates players while displaying a proposal to create certain players,
+        the number of which declines depending on the number of the player being created,
+        Then an object is created and added to the list of players
+
+        '''
+
+        # Generate the sentences of creating the players
+        for i in range(1, self.count_of_players + 1):
+            if i == 1:
+                string_of_count = f"{i}-st"
+            elif i == 2:
+                string_of_count = f"{i}-nd"
+            else:
+                string_of_count = f"{i}-th"
+
+            # Request for first and last name
             name = input(f"What's the name of the {string_of_count} player: ")
             surname = input(f"What's the surname of the {string_of_count} player: ")
 
+            # creating an instance of the player class, with the given first and last name, respectively
             player = Player(name, surname)
             self.players.append(player)
 
+    # Function to get the player's info
     def get_players(self):
+        '''
+
+        This function displays data about players (Displays data from the list of players), namely:
+        1) Name
+        2) Surname
+        3) Score
+
+        '''
         for player in self.players:
             print(player.get_info())
 
+    # Functions for displaying texts
     @staticmethod
     def print_hello_text():
         print("/*------------------------------*/")
@@ -66,43 +135,95 @@ class Lottery(object):
         print("and how many rounds will there be.")
         print("Well, good luck to you, my dear)")
 
+    # Function to start the Game (Main logical function in this Class)
     def play(self):
+        '''
 
+        A single function that creates the entire gameplay.
+
+        Function algorithm:
+        1) Creating a clear playing field with numbers from 1 to 20
+        and an empty set of numbers to be chosen by the player or randomly generated
+        2) The list of players is shuffled so that it is not known exactly who goes first
+        3) A playing field and an empty set are created for randomly generated numbers or guessed by the player.
+        4) The beginning of the game itself, lasting 'self.rounds' rounds
+        5) Start iterating through the list of players
+        6) Creating an empty set that will be filled with random numbers,
+        with which the results will be calculated and the choice is given whether numbers will be generated,
+        which are to be chosen randomly or by the player
+        7) In the case of a random selection of numbers,
+        the empty set is filled with random numbers provided that they are absent in the set
+        8) In the case of a user choice of numbers, the empty set is filled with the numbers entered by the user,
+        provided that they are not in the set. in the case of a user choice of numbers,
+        the empty set is filled with the numbers entered by the user, provided they are not in the set;
+        9) A set of numbers is filled in, with which the user's numbers must match
+        10) There is a comparison of matches between the final numbers selected by the lottery
+        and the numbers entered by users, a playing field is formed with crossed out numbers
+        and a set of matching user numbers and final numbers is issued
+        11) Calculation of issuing points to the player who made the move is performed, in case of a jackpot,
+        the method of issuing a large number of points is triggered
+        12) At the end of the game, the results of the players are given
+
+        '''
+
+        # Creation a playing field and an empty set are created
+        # for randomly generated numbers or guessed by the player.
         field = create_field(self.number_from_find)
-        shuffle(self.players)
         set_of_guess_numbers = set()
 
+        # Shuffle order of players
+        shuffle(self.players)
+
+        # The game starts according to the given rounds
         for _ in range(self.rounds):
+
+            # Start the iteration through the list of players
             for player in self.players:
+                # Formation of an empty set of given numbers by the player or random numbers
                 result_turn = set()
+
+                # Alert, whose turn it is
                 print(f"now it's turn of {player}")
                 print("Do you want to guess numbers on one's own?")
                 print("Type 0 here to have the game choose the numbers for you.")
-                numbers = int(input("Type here: "))
-                if numbers == 0:
+
+                # Player choice of number selection method
+                choice_to_create_numbers = int(input("Type here: "))
+                if choice_to_create_numbers == 0:
+                    # Random Number Generation Loop
                     while len(set_of_guess_numbers) < self.number_to_find:
                         num = randint(1, self.number_from_find)
                         set_of_guess_numbers.add(num)
 
                 else:
+                    # Beginning of the loop to add random counts in instead of the player's selection
                     while len(set_of_guess_numbers) < self.number_to_find:
+
+                        # Player number
                         num = int(input("Type here your number: "))
+
+                        # Checking the correctness of the entered data
                         if 0 < num <= self.number_from_find and (num not in set_of_guess_numbers):
                             set_of_guess_numbers.add(num)
                         else:
+                            # Notification of incorrectly entered data
                             print(f"Your number is too much bigger or smaller than {self.number_from_find}")
                             print("or maybe already in array of your guessed numbers")
                             print("Please type correct values!")
 
+                # Beginning of the player
                 while len(result_turn) < self.number_to_find:
                     num = randint(1, self.number_from_find)
                     result_turn.add(num)
 
+                # Displaying a field with filled numbers that the player guessed
                 result_of_result = get_beautiful_field(self.number_from_find, field, set_of_guess_numbers, result_turn)
 
+                # Get the result to the field with crossed out numbers, and random number generation result
                 beautiful_field = result_of_result[0]
                 result_of_matching = result_of_result[1]
 
+                # Check the number of matches, to add points
                 if not result_of_matching:
                     print('\n')
                     print("/*------------------------------------------------*/")
@@ -114,14 +235,26 @@ class Lottery(object):
                     print(f"you guessed {str(len(result_of_matching))} numbers!")
                     print("/*------------------------------------------------*/")
                     print('\n')
+
+                    # Adding points to the player making the move and getting
+                    # a field with guessed numbers crossed out
                     player.add_score(len(result_of_matching), 1)
                     get_field(beautiful_field)
-
                 else:
+
+                    # Jackpot message output
                     print("/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/")
                     print("/*YOU HAVE BEEN GUESSING ALL THE NUMBERS!!!*/")
                     print("/*              GET A JACKPOT              */")
+
+                    # Adding a large number of points to the player making the move and getting
+                    # a field with guessed numbers crossed out
                     player.add_super_score()
                     get_field(beautiful_field)
 
+        # Call the function for getting info about the players
         self.get_players()
+
+    # Magic function 'Destructor'
+    def __del__(self):
+        print("The end of the Game")
--- player.py
+++ player.py
@@ -1,18 +1,32 @@
 from idcreator import Id
 
+
+# ID object
 id = Id()
 
 
 class Player(object):
-    def __init__(self, name: str, surname: str, score=0):
-        self.__id = id.generate_id()
-        self.__name = name
-        self.__surname = surname
-        self.__score = score
 
+    # The validate function
+    @staticmethod
+    def validation_of_name_and_surname(check_name: str, check_surname: str):
+        return check_name and check_surname
+
+    # Magic function for initialization class object
+    def __init__(self, name: str, surname: str, score=0):
+        if Player.validation_of_name_and_surname(name, surname):
+            self.__id = id.generate_id()
+            self.__name = name
+            self.__surname = surname
+            self.__score = score
+        else:
+            pass
+
+    # Get result function
     def get_info(self):
         return f"{self.__name} {self.__surname} has the {self.__score} points"
 
+    # Adding points for achievements in the game
     def add_score(self, multiplier=1, outp=0):
         self.__score = self.__score + 100 * multiplier
         if outp:
@@ -21,12 +35,15 @@ class Player(object):
         else:
             pass
 
+    # Adding points for special achievements in the game
     def add_super_score(self):
         self.__score += 20000
 
+    # View representations of a class object for users
     def __str__(self):
         return f"Player {self.__name} {self.__surname}"
 
+    # View representations of a class object for developers
     def __repr__(self):
         return f"<Player: id: {self.__id }, name:'{self.__name}'," \
                f" surname:'{self.__surname}', score:{self.__score}>"
