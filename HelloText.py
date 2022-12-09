fa777f58bd4478b86873eb9432f43476c294564f
Mihail <viktor.svorikin@gmail.com>
2022-12-10 00:35:14 +0100

    description added

--- HelloText.py
+++ HelloText.py
@@ -1,3 +1,11 @@
+'''
+
+Separate document for sample text output function
+
+'''
+
+
+# The function of printing the welcome text
 def print_hello_text():
     print("/*____________________________Hello dear player! :)____________________________*/")
     print("I'm glad to welcome in my collection of mini-games, Here are the following games:")
